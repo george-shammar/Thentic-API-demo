de611f2887417222a93f09ddced8a1e026c305f2
george-shammar <george.gbenle@outlook.com>
2022-09-21 22:34:13 +0100

    Create deploy button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 
 function App() {
+
+  function deploy() {
+    
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Thentic API Demo</h1>
-        
+        <button onClick = {() => deploy()}>
+          Deploy Contract
+        </button>
       </header>
     </div>
   );
