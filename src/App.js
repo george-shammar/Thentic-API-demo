bac21673a2243b4be6b4bc928705d1af1b98f4f0
george-shammar <george.gbenle@outlook.com>
2022-09-21 22:37:35 +0100

    Create new contract object

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import './App.css';
 
 function App() {
 
-  
+  const newContract = {
+    method: 'POST',
+    url: 'https://thentic.tech/api/nfts/contract',
+    headers: {'Content-Type': 'application/json'},
+    data: {
+      "key":"rOpgr7s5wA1mQtjGVUsQIRWHY9SDfLcl",
+      "chain_id":97,
+      "name":"Kilimanjaro",
+      "short_name":"KIL"}
+  }
 
   function deploy() {
     axios.request(newContract).then(function (response) {
