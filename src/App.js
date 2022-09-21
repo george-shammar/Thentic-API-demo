8fae2f8eb2733e3f8c106263f4b2e392d707a19d
george-shammar <george.gbenle@outlook.com>
2022-09-21 22:26:06 +0100

    Edit bootstrapped code

--- src/App.js
+++ src/App.js
@@ -5,15 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Thentic DEMO
-        </a>
+        <h1>Thentic API Demo</h1>
+        
       </header>
     </div>
   );
