c579dbe43026aeb9fefc9d1fa6bc3416ce95dedd
george-shammar <george.gbenle@outlook.com>
2022-09-22 17:17:01 +0100

    Set link url

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const axios = require("axios");
 
 function App() {
   const [status, setStatus] = useState("");
+  const [link, setLink] = useState("");
 
   const mintNFT = {
     method: 'POST',
@@ -14,18 +15,18 @@ function App() {
             "key":"rOpgr7s5wA1mQtjGVUsQIRWHY9SDfLcl",
             "chain_id":97,
             "contract":"0x19d9f60e4cf3256a056b6f818425ce1bacd62f3d",
-            "nft_id":2,
+            "nft_id":3,
             "nft_data":"data",
             "to":"0x909045516Ee992b9A8FF98b2613CE71e2b2B91ad"
           }
   }
 
   function mint() {
-    setStatus("Getting started...")
     axios.request(mintNFT).then(function (response) {
       console.log(response.data.transaction_url);
       const url = response.data.transaction_url
-     setStatus()
+      setLink(url);
+      setStatus("Minting NFT through Thentic...")
     }).catch(function (error) {
       console.error(error);
     });
@@ -33,16 +34,24 @@ function App() {
     console.log("minted");
   }
 
-  const confirmedStatus =  "Getting started...";
+  const confirmedStatus =  "Minting NFT through Thentic...";
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Thentic API Demo</h1>
         <button onClick = {() => mint()}>
-          Mint NFT
+          Get Started
         </button>
         <div>{status}</div>
+        <div>
+        {status ===  confirmedStatus ? (
+            <a href={link} target="_blank" >Mint NFT</a>
+          ) : (
+            <p></p>
+        )}
+        </div>
+        
       </header>
       
     </div>
