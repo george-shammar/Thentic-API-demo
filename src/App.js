ebf06016dfdd601c150a0672e7ff75c6a4a4f67a
george-shammar <george.gbenle@outlook.com>
2022-09-22 16:41:46 +0100

    Fetch url

--- src/App.js
+++ src/App.js
@@ -1,67 +1,8 @@
-import { init, useConnectWallet } from '@web3-onboard/react';
-import injectedModule from '@web3-onboard/injected-wallets';
-import Onboard from '@web3-onboard/core';
-import coinbaseWalletModule from '@web3-onboard/coinbase';
-import walletConnectModule from '@web3-onboard/walletconnect';
-import { ethers } from 'ethers'
 import './App.css';
 const axios = require("axios");
 
 
-
-const injected = injectedModule();
-const rpcApiKey = 'ALCHEMY_KEY'
-// const rpcUrl = `https://eth-mainnet.g.alchemy.com/v2/${rpcApiKey}` || `https://mainnet.infura.io/v3/${rpcApiKey}`
-const rpcUrl = `https://polygon-mumbai.g.alchemy.com/v2/${rpcApiKey}`
-
-// initialize Onboard
-init({
-  wallets: [injected],
-  chains:[
-    {
-      id: '80001',
-      token: 'MATIC',
-      label: 'Polygon mumbai',
-      rpcUrl
-    }
-  ]
-})
-
-const coinbaseWalletSdk = coinbaseWalletModule();
-const walletConnect = walletConnectModule();
-
-const onboard = Onboard({
-  wallets: [
-    injected,
-    coinbaseWalletSdk,
-    walletConnect
-  ],
-  chains:[
-    {
-      id: '80001',
-      token: 'MATIC',
-      label: 'Polygon mumbai',
-      rpcUrl
-    }
-  ]
-})
-
-
 function App() {
-
-  const [{ wallet, connecting }, connect, disconnect] = useConnectWallet();
-
-  // create an ethers provider
-  let ethersProvider;
-
-  if (wallet) {
-    ethersProvider = new ethers.providers.Web3Provider(wallet.provider, 'any')
-  }
-
-  async function connectWallet() {
-    const connectedWallets = await onboard.connectWallet();
-    console.log(connectedWallets);
-  }
 
 
   const mintNFT = {
@@ -70,9 +11,9 @@ function App() {
     headers: {'Content-Type': 'application/json'},
     data: {
             "key":"rOpgr7s5wA1mQtjGVUsQIRWHY9SDfLcl",
-            "chain_id":80001,
-            "contract":"0x1c1deb32085a4bd52a4974f841803daaa595deb2",
-            "nft_id":1,
+            "chain_id":97,
+            "contract":"0x19d9f60e4cf3256a056b6f818425ce1bacd62f3d",
+            "nft_id":2,
             "nft_data":"data",
             "to":"0x909045516Ee992b9A8FF98b2613CE71e2b2B91ad"
           }
@@ -80,7 +21,8 @@ function App() {
 
   function mint() {
     axios.request(mintNFT).then(function (response) {
-      console.log(response.data);
+      console.log(response.data.transaction_url);
+      const url = response.data.transaction_url
     }).catch(function (error) {
       console.error(error);
     });
@@ -91,12 +33,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <button
-                // disabled={connecting} onClick={() => (wallet ? disconnect() : connect())}
-                disabled={connecting} onClick = {() => connectWallet()}
-              >
-                {connecting ? 'connecting' : wallet ? 'disconnect' : 'Connect wallet'}
-              </button>
         <h1>Thentic API Demo</h1>
         <button onClick = {() => mint()}>
           Mint NFT
